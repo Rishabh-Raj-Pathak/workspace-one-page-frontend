224017089307c7a04b82b849a5425020ecf1ed58
Rishabh Raj Pathak <rishabhrajpathak06@gmail.com>
2025-09-17 00:15:29 +0530

    repo initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero'
+import Container from './components/Container'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  )
+}
+
+export default App
